b72e3ca0e8d59331ca57d2b6cf774d2d30c054cb
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2024-10-20 22:57:56 +0530

    started working on sponsoships component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,10 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+import colors from "tailwindcss/colors";
+
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+
 
 const config: Config = {
   content: [
@@ -8,12 +14,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        scroll:
+            "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+
 export default config;

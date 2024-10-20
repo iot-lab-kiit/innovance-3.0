e26053c09333e93ae297c8528b71488852abd37d
Vidhi425 <hcpsvidhi25@gmail.com>
2024-10-20 21:43:40 +0530

    event-schedule

--- app/components/EventSchedule/EventSchedule.tsx
+++ app/components/EventSchedule/EventSchedule.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { MdLocationPin } from "react-icons/md";
 import { motion } from "framer-motion";
 

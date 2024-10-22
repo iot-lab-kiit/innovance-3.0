8080380fd2e09a839f9623dfa5764b317649394c
Archi Shaw <archishaw2004@gmail.com>
2024-10-22 14:50:34 +0530

    about

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero/Hero";
 import StatisticsShowcase from "./components/StatisticsShowcase/StatisticsShowcase";
 import TimerSection from "./components/Timer/TimerSection";
 import { EventSchedule } from "./components/EventSchedule/EventSchedule";
-
+import about from "./components/global/about";
 export default function Home() {
   return (
     <>

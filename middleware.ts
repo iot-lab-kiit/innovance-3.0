904caa5c5186eccee4571aaba1f5469b5ca87fad
Akangkha <akangkhasarkar@gmail.com>
2024-11-05 11:36:07 +0530

    Middleware update

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+const protectedPath = "/register-internal-2411";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  if (pathname.startsWith(protectedPath)) {
+    const authToken = request.cookies.get("auth-token");
+    if (!authToken) {
+      const url = new URL("/", request.url);
+      return NextResponse.redirect(url);
+    }
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/register-internal-2411"], 
+};

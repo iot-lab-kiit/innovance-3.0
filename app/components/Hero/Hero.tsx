3e057e3d9dffbf2b922b9c478bf4d05fe057cc6f
KingsukNandi <kingsuk24nandi@gmail.com>
2024-11-06 17:37:41 +0530

    Fix hero section button

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,15 +1,21 @@
 import Marquee from "react-fast-marquee";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { useState } from "react";
+// import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Hero() {
-  const [isHovered, setIsHovered] = useState(false);
+  // const [isHovered, setIsHovered] = useState(false);
+  // useEffect(() => {
+  //   setIsHovered(false);
+  // }, []);
 
   return (
     <div className="h-[80vh] md:h-screen bg-[#101010]">
-      <div id="hero" className="h-[80vh] lg:h-3/4 flex justify-center items-center">
+      <div
+        id="hero"
+        className="h-[80vh] lg:h-3/4 flex justify-center items-center"
+      >
         <div className="text-center flex flex-col gap-8">
           <motion.div
             className=" text-blue-500 text-2xl font-bitter"
@@ -17,7 +23,7 @@ export default function Hero() {
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, ease: [0.17, 0.55, 0.55, 1] }}
           >
-           &#8226;   9th-10th November, 2024 &#8226; 
+            &#8226; 9th-10th November, 2024 &#8226;
           </motion.div>
           <motion.div
             initial={{ y: 100, opacity: 0 }}
@@ -29,11 +35,11 @@ export default function Hero() {
           </motion.div>
           <div
             className="relative w-fit m-auto"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
+            // onMouseEnter={() => setIsHovered(true)}
+            // onMouseLeave={() => setIsHovered(false)}
           >
             <Link href="/registrations">
-              <motion.div
+              {/* <motion.div
                 initial={{ width: "100%", y: 100, opacity: 0 }}
                 animate={
                   isHovered
@@ -42,30 +48,17 @@ export default function Hero() {
                 }
                 transition={{ duration: 0.5, ease: [0.17, 0.55, 0.55, 1] }}
                 className="absolute w-full h-full bg-blue-500"
-              ></motion.div>
-              <motion.button
-                initial={{ y: 100, opacity: 0 }}
-                animate={
-                  isHovered
-                    ? {
-                        color: "#3b82f6",
-                        borderColor: "#3b82f6",
-                        y: 0,
-                        opacity: 1,
-                      }
-                    : { y: 0, opacity: 1 }
-                }
-                transition={{ duration: 0.5, ease: [0.17, 0.55, 0.55, 1] }}
-                whileHover={{ color: "#3b82f6", borderColor: "#3b82f6" }}
-                className="text-xs sm:text-base hover:font-semibold text-background border-background border w-fit m-auto py-3 px-7 font-geistMono"
-              >
+              ></motion.div> */}
+              <button className="text-xs sm:text-base hover:scale-105 transition-all text-background bg-blue-500 border-background border w-fit m-auto py-3 px-7 font-geistMono">
                 GET TICKET
-              </motion.button>
+              </button>
             </Link>
           </div>
         </div>
       </div>
-      <div id="marquee" className="font-geistMono mt-2 md:mt-0"> {/* Adjusted margin */}
+      <div id="marquee" className="font-geistMono mt-2 md:mt-0">
+        {" "}
+        {/* Adjusted margin */}
         <div className="w-[calc(100vw+8px)] -left-[6px] -skew-x-[10deg] -rotate-[10deg] sm:-rotate-6 sm:-skew-x-6 md:-rotate-[4deg] md:-skew-x-[4deg] lg:-rotate-3 lg:-skew-x-3 bg-blue-500 absolute z-20 shadow-[0px_0px_10px_10px_#00000024]">
           <Marquee
             speed={30}
